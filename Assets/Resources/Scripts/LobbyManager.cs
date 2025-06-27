190a9f1d3fa38c724c334dd9112fae7f1852df82
Flo <floriangodfroid@outlook.fr>
2025-06-28 01:22:50 +0200

    Requests

--- Assets/Resources/Scripts/LobbyManager.cs
+++ Assets/Resources/Scripts/LobbyManager.cs
@@ -3,64 +3,154 @@ using System.Linq;
 using Newtonsoft.Json.Linq;
 using NativeWebSocket;
 using Unity.VisualScripting;
+using UnityEditor.PackageManager.Requests;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using UnityEditor;
 
 public class LobbyManager : GameSingleton
 {
     public WebSocket websocket;
-    
-    async void Start()
+
+    JObject jsonResponse;
+    Dictionary<string, Action<JObject>> responses;
+    bool isConnected;
+
+    private void Awake()
+    {
+        responses = new Dictionary<string, Action<JObject>>()
+        {
+            {"create_user", OnUserCreated },
+            {"create_lobby", OnLobbyCreated },
+            {"try_join_lobby", OnTryJoinLobby},
+            {"leave_lobby", OnLeaveLobby},
+            {"get_lobbies", OnLobbiesFetch},
+            {"get_data", OnDataFetch},
+            {"set_data", OnDataSet}
+        };
+    }
+     
+    void Update()
+    {
+#if !UNITY_WEBGL || UNITY_EDITOR
+        if (isConnected)
+        {
+            websocket.DispatchMessageQueue();
+        }
+#endif
+    }
+
+    private void OnDataSet(JObject response)
+    {
+        bool exist = response["exist"].ToObject<bool>();
+    }
+
+    private void OnDataFetch(JObject response)
+    {
+        bool exist = response["exist"].ToObject<bool>();
+        string val = response["val"].ToString();
+    }
+
+    private void OnLobbiesFetch(JObject response)
+    {
+        JObject[] lobbies = response["lobbies"].ToObject<JObject[]>();
+    }
+
+    private void OnLeaveLobby(JObject response)
+    {
+        bool left = response["left"].ToObject<bool>();
+    }
+
+    private void OnTryJoinLobby(JObject response)
+    {
+        bool joined = response["joined"].ToObject<bool>();
+        string message = response["message"].ToString();
+    }
+
+    private void OnLobbyCreated(JObject response)
+    {
+        string lobbyId = response["lobby_id"].ToString();
+    }
+
+    private void OnUserCreated(JObject response) 
+    {
+        string uuid = response["user_id"].ToString();
+        OnLog($"new uuid received : {uuid}", LoggingSeverity.Message);
+    }
+
+    //to do refacturisé grace à L'UIManager
+
+    public async void Connect(string username)
     {
         websocket = new WebSocket("ws://localhost:3030");
 
         websocket.OnOpen += () =>
         {
-            Debug.Log("Connection open!");
+            ToWSS(new()
+            {
+                {"request_method", "create_user" },
+                {"user_name", username }
+            });
+            Debug.Log("Connected !");
+            isConnected = true;
         };
 
         websocket.OnError += (e) =>
         {
             Debug.Log("Error! " + e);
+            isConnected = false;
         };
 
-        websocket.OnClose += (e) =>
-        {
-            OnLog($"Connection closed! => {e}", LoggingSeverity.Error);
-        };
+        websocket.OnClose += OnClose;
 
-        websocket.OnMessage += (bytes) =>
-        {
-            Debug.Log("OnMessage!");
-            Debug.Log(bytes);
-
-            // getting the message as a string
-            // var message = System.Text.Encoding.UTF8.GetString(bytes);
-            // Debug.Log("OnMessage! " + message);
-        };
+        websocket.OnMessage += OnMessage;
 
         // waiting for messages
         await websocket.Connect();
-
-        JObject jobj = new JObject();
-        jobj["sdsd"] = new JObject() { { "age", 13 }, {"pute", new JObject() } };
     }
 
-    private void Update()
+    void ToWSS(JObject jsonRequest)
     {
-#if !UNITY_WEBGL || UNITY_EDITOR
-        websocket.DispatchMessageQueue();
-#endif
+        websocket.SendText(jsonRequest.ToString());
+        OnLog($"Sent Message of method {jsonRequest["request_method"]}", LoggingSeverity.Info);
     }
 
-    async void SendWebSocketMessage()
+    public void TryJoinLobby()
     {
-        if (websocket.State == WebSocketState.Open)
+        //get lobbies;
+        //ToWSS(new()
+        //{
+
+        //});
+        //connect to available lobby;
+        //create lobby;
+    
+    }
+
+    void OnMessage(byte[] bytes)
+    {
+        string message = System.Text.Encoding.UTF8.GetString(bytes);
+        JObject jsonResponse = JObject.Parse(message);
+
+        OnLog($"Message Received -- Content : {jsonResponse}");
+
+        if (responses.ContainsKey(jsonResponse["request_method"].ToString()))
         {
-            // Sending bytes
-            await websocket.Send(new byte[] { 10, 20, 30 });
-
-            // Sending plain text
-            //await websocket.SendText("plain text message");
+            responses[jsonResponse["request_method"].ToString()](jsonResponse);
         }
+        else
+        {
+            OnLog("No method was found", LoggingSeverity.Warning);
+        }
+
+    }
+
+    void OnClose(WebSocketCloseCode e)
+    {
+        OnLog($"Connection closed! => {e}", LoggingSeverity.Error);
+        isConnected = false;
+
     }
 
     private async void OnApplicationQuit()
--- Assets/Resources/Scripts/UIManager.cs
+++ Assets/Resources/Scripts/UIManager.cs
@@ -9,17 +9,24 @@ public class UIManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI authLabel, lobbyPlayersCount;
     [SerializeField] GameObject joinLobbyButton, authButton;
     [SerializeField] GameObject lobbyContainer, authContainer;
+    [SerializeField] TMP_InputField usernameInput;
     [SerializeField] UserSlot[] userSlots;
 
-
+    LobbyManager lobbyManager;
 
     public void Start()
     {
-
+        lobbyManager = GameSingleton.GetInstance<LobbyManager>();
 
     }
 
-
+    public void Connect()
+    {
+        if(usernameInput.text.Length > 3)
+        {
+        lobbyManager.Connect(usernameInput.text);
+        }
+    }
 
     void DisplayLobby()
     {
