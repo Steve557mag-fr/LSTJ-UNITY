9d9f357b8f3c57116712aa49c98a569319a7ad73
Steve557mag-fr <cbuchelotsteve557@gmail.com>
2025-07-11 10:30:00 +0200

    add mg singleton + rename scenes

--- Assets/Resources/Scripts/MinigamesManager.cs
+++ Assets/Resources/Scripts/MinigamesManager.cs
@@ -1,14 +1,42 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MinigamesManager : GameSingleton
 {
 
+    public MinigameInfo[] minigamesInfo;
+    protected Dictionary<string, MinigameInfo> minigames = new();
+
+    protected string currentMGName;
 
     internal BaseMinigame GetCurrentMG()
     {
-        return null;
+        return FindFirstObjectByType<BaseMinigame>();
     }
-        
+
+    internal void SetMG(string newMG)
+    {
+        if(minigames.ContainsKey(newMG)) currentMGName = newMG;
+    }
+
+    private void Awake()
+    {
+        for(int i = 0; i < minigamesInfo.Length; i++)
+        {
+            minigames.Add(minigamesInfo[i].minigameName, minigamesInfo[i]);
+        }
+    }
     
+}
+
+
+
+[Serializable]
+public struct MinigameInfo
+{
+    public string minigameName;
+    public string sceneMinigame;
 
 }
